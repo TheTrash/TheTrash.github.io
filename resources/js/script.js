6045552d06d8ff7bb21cfb0a1ebf084a3cd3c350
TheTrash <TheTrash@users.noreply.github.com>
2022-11-30 15:02:22 +0100

    Feedback Update!

---
+++ resources/js/script.js
@@ -0,0 +1,24 @@
+
+clicked = 0
+$("#like_button").click(function(){
+    if(clicked == 0){
+        $(this).attr('class', 'fa-solid fa-heart');
+        clicked = 1;
+        add_feedback();
+    }
+  });
+
+function add_feedback(){
+    $.ajax({
+		type: "POST",
+		url: "http://thetrash.altervista.org/feedback/add_feedback.php",
+        
+        data: {"article": "night-city" },
+		success: function(msg, status, jqXHR){
+            $("#response").html(" Grazie!");
+		},
+		error: function(jqXHR, status, errorThrown){
+            $("#response").html(" errore!");
+		}
+	});
+}
